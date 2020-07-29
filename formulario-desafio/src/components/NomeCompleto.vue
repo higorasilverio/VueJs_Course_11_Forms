c0e433cf8ffeb2e7865829b076b30b4b674f7453
higorasilverio <higorasilverio@outlook.com>
2020-07-29 16:33:18 -0300

    Final files for forms class

---
+++ formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <Rotulo nome="Nome">
+      <input @input="alterarNome" type="text" :value="value.nome" />
+    </Rotulo>
+    <Rotulo nome="Sobrenome">
+      <input @input="alterarSobrenome" type="text" :value="value.sobrenome" />
+    </Rotulo>
+  </div>
+</template>
+
+<script>
+import Rotulo from "./Rotulo.vue";
+export default {
+  props: {
+    value: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: { Rotulo },
+  methods: {
+    alterarNome(event) {
+      this.$emit("input", {
+        nome: event.target.value,
+        sobrenome: this.value.sobrenome,
+      });
+    },
+    alterarSobrenome(event) {
+      this.$emit("input", {
+        nome: this.value.nome,
+        sobrenome: event.target.value,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
